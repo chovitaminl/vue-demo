305ee5b1c4e669d884fb821152ebc425c1f6c0cc
liejiayong <809206619@qq.com>
2018-01-21 21:42:47 +0800

    updata vue-music components player and vuex defination

---
+++ vue-music/src/components/player/player.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="player" v-show="playlist.length">
+    <transition name="normal">
+      <div class="normal-player" v-show="fullScreen"></div>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen"></div>
+    </transition>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters([
+        'currentIndex',
+        'fullScreen',
+        'playing'
+      ])
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .player
+    .normal-player
+      position: fixed
+      top: 0
+      bottom: 0
+      left: 0
+      right: 0
+      z-index: 150
+      background-color: $color-background
+      &.normal-enter-active,&.normal-leave-active
+        transition: all 0.4s
+      &.normal-enter,&.normal-leave-to
+        opacity: 0
+    .mini-player
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background-color: $color-highlight-background
+      &.min-enter-active,&.min-leave-active
+        transition: all 0.4s
+      &.min-enter,&.mini-leave-to
+        opacity: 0
+</style>
--- vue-music/src/store/mutations.js
+++ vue-music/src/store/mutations.js
@@ -3,6 +3,30 @@ import * as types from './mutation-types'
 const mutations = {
   [types.SET_SINGER] (state, singer) {
     state.singer = singer
+  },
+  [types.SET_PLAYING] (state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN] (state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAYLIST] (state, list) {
+    state.playlist = list
+  },
+  [types.SET_SEQUENCE_LIST] (state, list) {
+    state.sequenceList = list
+  },
+  [types.SET_MODE] (state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CUREET_INDEX] (state, index) {
+    state.currentIndex = index
+  },
+  [types.SET_DISC] (state, disc) {
+    state.disc = disc
+  },
+  [types.SET_TOP_LIST] (state, topList) {
+    state.topList = topList
   }
 }
 
