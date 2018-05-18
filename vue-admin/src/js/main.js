1fcf440a8f6c9ccea4e56a981e8b37d63a313060
liejiayong <809206619@qq.com>
2018-05-18 23:56:34 +0800

    format iview space

--- vue-admin/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Main from '../view/Main.vue'
-import Router from './router'
-
-const app = new Vue({
-  el: '#main',
-  Router,
-  render: h => h(Main)
-})
--- vue-admin/src/js/router.js
+++
@@ -1,9 +0,0 @@
-import routes from './routes'
-import VewRouter from 'vue-router'
-
-const router = new VueRouter({
-  mode: 'hash',
-  routes
-})
-
-export default router
